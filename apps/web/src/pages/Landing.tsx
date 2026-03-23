491968b0d2356f8b72d3b6239ce7765f55e6f203
AndyThai1000 <andy.thai54687@gcccd.edu>
2026-03-23 11:23:52 -0700

    3232026

--- apps/web/src/pages/Landing.tsx
+++ apps/web/src/pages/Landing.tsx
@@ -19,7 +19,7 @@ function LandingPage() {
         <nav className="nav-links">
           <button className="nav-btn">Classes</button>
           <button className="nav-btn">Dashboard</button>
-          <button className="nav-btn primary">SIGN IN</button>
+          <button className="nav-btn primary" onClick={routeSignin}>SIGN IN</button>
           <button className="nav-prof-btn" onClick={() => window.location.reload()}>
             <img src="/profileIcon.svg" alt="Profile" className="prof-icon" />
           </button>
@@ -30,7 +30,7 @@ function LandingPage() {
         <div className="hero-content">
           <h1>FORMING CONNECTIONS</h1>
           <p>Be able to connect with your peers much easier and faster.</p>
-          <button className="cta-btn">Create Account</button>
+          <button className="cta-btn" onClick={routeSignin}>Create Account</button>
         </div>
       </main>
     </div>
--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Landing from "../pages/Landing";
+import SignIn from "../pages/SignIn"
 import Error from "../pages/Error";
 
 export const router = createBrowserRouter([
@@ -7,7 +8,8 @@ export const router = createBrowserRouter([
     path: "/",
     errorElement: <Error />,
     children: [
-      { index: true, element: <Landing /> }
+      { index: true, element: <Landing /> },
+      { path: "signin", element: <SignIn /> }
     ],
   },
 ]);
